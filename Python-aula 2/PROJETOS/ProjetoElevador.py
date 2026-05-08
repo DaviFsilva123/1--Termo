a47cbed81628a43676b4ab01340bce95f9a0eaf9
Davi Ferreira <Sesisenaisp@gmail.com>
2026-05-08 11:20:43 -0300

    Iniciando projeto - Elevador

---
+++ Python-aula 2/PROJETOS/ProjetoElevador.py
@@ -0,0 +1,18 @@
+# O sistema de elevador do prédio
+# O prédio possui  10 andares, sendoo térreo o andar 0. O elevador pode se mover para cima e para baixo, e tem a capacidade de transportar até 5 pessoas
+# O elevador começa no andar 0 e pode ser chamado por qualquer pessoa em qualquer andar
+# O elevador deve se mover para o andar onde a pessoa chamou, e depois para o andar destino da pessoa.
+# O elevador deve exibir mensagens indicando o andar atual, o número de pessoas no elevador, e as ações realizadas (subindo, descendo, parando). O programa deve continuar rodando até que o usuário decida encerrar.
+
+# Levantamento de Requisitos
+# Requisitos funcionais (RF):
+# - O elevador deve se mover para cima e para baixo.
+# - Precisa possuir um sistema que conte andares de 0 a 10.
+# - O elevador deve transportar até 5 pessoas.
+# - O elevador pode ser chamado de qualquer andar do andar 0 até o andar 10.
+# - O elevador precisa se mover do ponto em que foi chamado até o destino de quem chamou.
+# Requisitos não fincional (RNF):
+# - Exibir mensagens indicando o andar atual.
+# - Exibir o número de pessoas no elevador.
+# - As ações realizadas (Subindo, descendo, parando).
+
